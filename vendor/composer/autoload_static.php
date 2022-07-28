f14dca04ef62a06b4eb98e9dc6982f6c9b2dcb04
tuzlu07x <fatihtuzlu07@icloud.com>
2022-07-28 22:26:07 +0300

    composer updated.

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -17,7 +17,12 @@ class ComposerStaticInit0ff8df18244f285b3ae5f5fac1261ce4
         'EFINANS\\' => 
         array (
             0 => __DIR__ . '/../..' . '/src',
+            1 => __DIR__ . '/../..' . '/src',
         ),
+    );
+
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -25,6 +30,7 @@ class ComposerStaticInit0ff8df18244f285b3ae5f5fac1261ce4
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit0ff8df18244f285b3ae5f5fac1261ce4::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit0ff8df18244f285b3ae5f5fac1261ce4::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInit0ff8df18244f285b3ae5f5fac1261ce4::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'muhittingulap/qnbfinansbank',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '5b486eb2d4e9a4ad2531b547050920872a011e28',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'muhittingulap/qnbfinansbank' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '5b486eb2d4e9a4ad2531b547050920872a011e28',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
